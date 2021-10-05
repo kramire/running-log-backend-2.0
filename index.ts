ec99d20566a21357f948fd44c3eecd2e696fe2ef
kramire <kramire@users.noreply.github.com>
2021-10-05 11:42:30 +0200

    feat: config cors

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import router from './router';
 import sequelize from './models';
+import cors from 'cors';
 
-const PORT = '3000';
+const PORT = '4000';
 
 const app = express();
 
 sequelize.sync().then((res) => {
+  app.use(cors({ origin: 'http://localhost:3000' }));
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use('/', router);
